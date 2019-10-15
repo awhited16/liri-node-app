6210c468cc9b541d544a4ce62ad0da6f35a2dbf6
awhited16 <amw157@case.edu>
2019-10-14 22:22:58 -0400

    update all functions, clean up and test

--- liri.js
+++ liri.js
@@ -16,157 +16,149 @@ var spotify = new Spotify(keys.spotify);
 var action = process.argv[2];
 
 switch (action) {
-    case "concert-this":
-      concert();
-      break;
+  case "concert-this":
+    concert();
+    break;
+  
+  case "spotify-this-song":
+    spotifySong();
+    break;
+  
+  case "movie-this":
+    movie();
+    break;
+  
+  case "do-what-it-says":
+    doThis();
+    break;
+  }
+
+  function concert() {
+    var artist = process.argv[3];
+    var bandQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    console.log(bandQueryURL);
+
+    axios.get(bandQueryURL).then(
+      function(response) {
+        console.log("here");
+        for (i = 0; i = response.length; i++) {
+          var venue = response[i].venue.name
+          var venueLoc = response[i].venue.city
+          var eventDate = response[i].datetime.splice(0,10);
+          console.log("Venue: " + venue);
+          console.log("Venue Locaction: " + venueLoc);
+          console.log("Event Date: " + eventDate);
+        };
+      })
+      .catch(function(error) {
+          if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+          } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+          } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          }
+          console.log(error.config);
+      });
+      
+      // fs.readFile("bank.txt", "utf8", function(err, data) {
+      //     if (err) {
+      //       return console.log(err);
+      //     }
+  };
+
+
+  function spotifySong() {
+    var song = process.argv[3];
+
+    spotify.search({
+      type: 'track', query: song
+    }).then(function(response) {
+      console.log("made it");
+      console.log(response);
+      for (j = 0; j = response.length; j++) {
+        console.log(response.tracks.href.items[j]);
+        // var artist = response.
+        // var songTitle = 0
+        // var link = 0
+        // var album = 0
+        // console.log("Artist: " + artist);
+        // console.log("Song: " + songTitle);
+        // console.log("Preview Link: " + link);
+        // console.log("Album: " + album);
+      };
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
+
+  function movie() {
+    var movieTitle = process.argv[3];
+    var OMDBQueryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(OMDBQueryURL).then(
+      function(response) {
+        for (k = 0; k = response.length; k++) {
+          console.log(response[k]);
+          var title = response[k].Title 
+          var IMDBRating = response[k].Ratings[0].Value
+          var RTRating = response[k].Ratings[1].Value
+          var country = response[k].Country
+          var language = response[k].Language
+          var plot = response[k].Plot
+          var actors = response[k].Actors
+          console.log("Movie Title: " + title);
+          console.log("IMDB Rating: " + IMDBRating);
+          console.log("Rotten Tomatoes Rating: " + RTRating);
+          console.log("Country: " + country);
+          console.log("Language: " + language);
+          console.log("Plot: " + plot);
+          console.log("Actors: " + actors);
+        }
+      })
+    .catch(function(error) {
+      if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+      } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+      } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
     
-    case "spotify-this-song":
-      spotify();
-      break;
-    
-    case "movie-this":
-      movie();
-      break;
-    
-    case "do-what-it-says":
-      doThis();
-      break;
-    }
+  };
 
-    function concert() {
-        var artist = process.argv[3];
-        var bandQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-        console.log(bandQueryURL);
-
-        axios.get(bandQueryURL).then(
-            function(response) {
-              for (i = 0; i = response.length; i++) {
-                var venue = response[i].venue.name
-                var venueLoc = response[i].venue.city
-                var eventDate = response[i].datetime.splice(0,10);
-                console.log("Venue: " + venue);
-                console.log("Venue Locaction: " + venueLoc);
-                console.log("Event Date: " + eventDate);
-              };
-            })
-        .catch(function(error) {
-            if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-            } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-            } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-            }
-            console.log(error.config);
-        });
-        
-        // fs.readFile("bank.txt", "utf8", function(err, data) {
-        //     if (err) {
-        //       return console.log(err);
-        //     }
-    };
-
-
-    // function spotify() {
-    //     var song = process.argv[3];
-    //     var spotifyQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-    //     axios.get(spotifyQueryURL).then(
-    //         function(response) {
-    //             var artist = 0 
-    //             var songTitle = 0
-    //             var link = 0
-    //             var album = 0
-    //             console.log("Artist: " + artist);
-    //             console.log("Song: " + songTitle);
-    //             console.log("Preview Link: " + link);
-    //             console.log("Album: " + album);
-    //         })
-    //     .catch(function(error) {
-    //         if (error.response) {
-    //         // The request was made and the server responded with a status code
-    //         // that falls out of the range of 2xx
-    //         console.log("---------------Data---------------");
-    //         console.log(error.response.data);
-    //         console.log("---------------Status---------------");
-    //         console.log(error.response.status);
-    //         console.log("---------------Status---------------");
-    //         console.log(error.response.headers);
-    //         } else if (error.request) {
-    //         // The request was made but no response was received
-    //         // `error.request` is an object that comes back with details pertaining to the error that occurred.
-    //         console.log(error.request);
-    //         } else {
-    //         // Something happened in setting up the request that triggered an Error
-    //         console.log("Error", error.message);
-    //         }
-    //         console.log(error.config);
-    //     });
-    // }
-
-    // function movie() {
-    //     var movie = process.argv[3];
-    //     var OMDBQueryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-    //     axios.get(OMDByQueryURL).then(
-    //         function(response) {
-    //             var title = response.Title 
-    //             var IMDBRating = response.Ratings[0].Value
-    //             var RTRating = response.Ratings[1].Value
-    //             var country = response.Country
-    //             var language = response.Language
-    //             var plot = response.Plot
-    //             var actors = response.Actors
-    //             console.log("Movie Title: " + title);
-    //             console.log("IMDB Rating: " + IMDBRating);
-    //             console.log("Rotten Tomatoes Rating: " + RTRating);
-    //             console.log("Country: " + country);
-    //             console.log("Language: " + language);
-    //             console.log("Plot: " + plot);
-    //             console.log("Actors: " + actors);
-    //          })
-    //     .catch(function(error) {
-    //         if (error.response) {
-    //         // The request was made and the server responded with a status code
-    //         // that falls out of the range of 2xx
-    //         console.log("---------------Data---------------");
-    //         console.log(error.response.data);
-    //         console.log("---------------Status---------------");
-    //         console.log(error.response.status);
-    //         console.log("---------------Status---------------");
-    //         console.log(error.response.headers);
-    //         } else if (error.request) {
-    //         // The request was made but no response was received
-    //         // `error.request` is an object that comes back with details pertaining to the error that occurred.
-    //         console.log(error.request);
-    //         } else {
-    //         // Something happened in setting up the request that triggered an Error
-    //         console.log("Error", error.message);
-    //         }
-    //         console.log(error.config);
-    //     });
-        
-    // };
-
-    // function doThis() {
-    //     fs.readFile("random.txt", "utf8", function(err, data) {
-    //         if (err) {
-    //           return console.log(err);
-    //         }
-    //         console.log("reading file")
-    //         spotify()
-    //     });
-    // };
+  function doThis() {
+      fs.readFile("random.txt", "utf8", function(err, data) {
+          if (err) {
+            return console.log(err);
+          }
+          console.log("reading file")
+          spotify()
+      });
+  };
 
 
     
